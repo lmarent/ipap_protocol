7d384026a49df6daa95114745d72e341d522fb0b
Luis Andres Marentes Cubillos <l_marent@yahoo.com>
2019-05-30 04:42:57 -0500

    remove cout message

--- src/IpAp_data_record_wrapper.cpp
+++ src/IpAp_data_record_wrapper.cpp
@@ -18,8 +18,6 @@ extern "C"
     void ipap_data_record_insert_field(ipap_data_record *data_record, 
                                        int eno, int ftype, ipap_value_field *value)
     {
-        cout << "In data record insert field" << endl;
-
         ipap_value_field *new_value = new ipap_value_field(*value);
         data_record->insert_field(eno,ftype,*new_value);
     }
